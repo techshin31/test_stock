d0878550e205343064b8a116d8a86e5954befc30
good593 <goodwon593@gmail.com>
2026-03-29 01:07:32 +0900

    작성중

--- etl/wics/utils.py
+++ etl/wics/utils.py
@@ -9,10 +9,19 @@ def get_yesterday(format='%Y%m%d'):
     yesterday = today - timedelta(days=1)
     return yesterday.strftime(format)
 
-def get_date_list(start_date:str, end_date:str, format='%Y%m%d'):
-    start_date = datetime.strptime(start_date, format)
-    end_date = datetime.strptime(end_date, format)
+def get_start_date(end_date:str, delta_days:int=365, format:str='%Y%m%d'):
+    start_date = datetime.strptime(end_date, format) - timedelta(days=delta_days)
+    return start_date.strftime(format)
+
+def get_date_list(start_date:str=None, end_date:str=None, format='%Y%m%d'):
+    if end_date is None:
+        end_date = get_yesterday(format)
+    if start_date is None:
+        start_date = get_start_date(end_date=end_date, delta_days=365*5, format=format)
+
     # 날짜 차이 계산
+    end_date = datetime.strptime(end_date, format)
+    start_date = datetime.strptime(start_date, format)
     delta = end_date - start_date
 
     # 리스트 컴프리헨션을 사용하여 문자열 리스트 생성
--- etl/wics/wics.py
+++ etl/wics/wics.py
@@ -1,5 +1,8 @@
+from tqdm.auto import tqdm
+from pandas import json_normalize
 import requests
 import pandas as pd
+import time
 
 def wics_url(date, wics_code):
     '''
@@ -28,3 +31,47 @@ def wics_json(date, wics_code):
     response = requests.get(url)
     return response.json()
 
+
+def save_wics_company(date_list, wics_medium_cds):
+    df_save = pd.DataFrame()
+
+    for date in tqdm(date_list, desc="date..", leave=True):
+        for wics_cd in tqdm(wics_medium_cds, desc="wics..", leave=False):
+            data_json = wics_json(date=date, wics_code=int(wics_cd))   
+            if not data_json['list']:    
+                continue  
+
+            df = json_normalize(data_json, record_path=['list'])
+            df['DATE'] = date
+            
+            # info 
+            df['INFO_MKT_VAL'] = data_json['info']['MKT_VAL']
+            df['INFO_TRD_AMT'] = data_json['info']['TRD_AMT']
+            df['INFO_CNT'] = data_json['info']['CNT']
+            # sector 
+            sector = [
+                sector for sector in data_json['sector'] if df['SEC_CD'].unique()[0] == sector['SEC_CD']
+            ][0]
+            df['SEC_RATE'] = sector['SEC_RATE']
+            df['IDX_RATE'] = sector['IDX_RATE']
+            # size 
+            for size in data_json['size']:
+                if size['SEC_CD'] == "WMI510":
+                    df['SIZE_WMI510'] = "WMI500 대형주"
+                    df['SIZE_WMI510_SEC_RATE'] = size['SEC_RATE']
+                    df['SIZE_WMI510_IDX_RATE'] = size['IDX_RATE']
+                elif size['SEC_CD'] == "WMI520":
+
+                    df['SIZE_WMI520'] = "WMI500 중형주"
+                    df['SIZE_WMI520_SEC_RATE'] = size['SEC_RATE']
+
+                    df['SIZE_WMI520_IDX_RATE'] = size['IDX_RATE']
+                elif size['SEC_CD'] == "WMI530":
+                    df['SIZE_WMI530'] = "WMI500 소형주"
+                    df['SIZE_WMI530_SEC_RATE'] = size['SEC_RATE']
+                    df['SIZE_WMI530_IDX_RATE'] = size['IDX_RATE']
+
+            df_save = pd.concat([df_save, df], axis=0)
+            time.sleep(0.01)
+
+    return df_save
